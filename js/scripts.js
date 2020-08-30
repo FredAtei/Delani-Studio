9d2103a129cdc167b9644dc56841df8dab941d29
Fred <akamaatei@gmail.com>
2020-08-30 18:28:41 +0300

    add jquery functions for pages

--- js/scripts.js
+++ js/scripts.js
@@ -9,4 +9,75 @@ $(document).ready(function () {
   });
 });
 
+$(document).ready(function () {
+  $("#development-image").click(function () {
+    $("#development-image").slideDown('1500').hide('1000');
+    $("#development").show('1500');
+  });
+  $("#development").click(function () {
+    $("#development").slideUp('1500');
+    $("#development-image").slideDown('1500');
+  });
+});
+
+$(document).ready(function () {
+  $("#product-image").click(function () {
+    $("#product-image").slideDown('1500').hide('1000');
+    $("#product").show('1500');
+  });
+  $("#product").click(function () {
+    $("#product").slideUp('1500');
+    $("#product-image").slideDown('1500');
+  });
+});
+
+$(document).ready(function () {
+  $("#work1").mouseover(function () {
+    $("#overlay").show();
+  }).mouseout(function () {
+    $("#overlay").hide();
+  });
+
+  $("#work2").mouseover(function () {
+    $("#overlay2").show();
+  }).mouseout(function () {
+    $("#overlay2").hide();
+  });
+
+  $("#work3").mouseover(function () {
+    $("#overlay3").show();
+  }).mouseout(function () {
+    $("#overlay3").hide();
+  });
+
+  $("#work4").mouseover(function () {
+    $("#overlay4").show();
+  }).mouseout(function () {
+    $("#overlay4").hide();
+  });
+
+  $("#work5").mouseover(function () {
+    $("#overlay5").show();
+  }).mouseout(function () {
+    $("#overlay5").hide();
+  });
+
+  $("#work6").mouseover(function () {
+    $("#overlay6").show();
+  }).mouseout(function () {
+    $("#overlay6").hide();
+  });
+
+  $("#work7").mouseover(function () {
+    $("#overlay7").show();
+  }).mouseout(function () {
+    $("#overlay7").hide();
+  });
+
+  $("#work8").mouseover(function () {
+    $("#overlay8").show();
+  }).mouseout(function () {
+    $("#overlay8").hide();
+  });
+});
 
